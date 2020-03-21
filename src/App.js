580e5d0379570a1c2b11e687f01e8ef419229c8c
Roger <thrasher_royerr666@hotmail.com>
2020-03-21 12:22:02 -0600

    Practicas con bedu de react routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route,
+  useLocation,
+  useParams
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <hr />
+        <Switch>
+          <Route path="/profile/:id?" component={HomePage} />
+          <Route path="/payment" component={Pay} />
+          <Route path="/batman" component={About} />
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
+
+const Nav = props => {
+  console.log(props);
+  let location = useLocation();
+  return (
+    <nav>
+      <ul>
+        <li className={` ${location.pathname === "/profile" ? "Active" : ""}`}>
+          <Link to="/profile">Home</Link>
+        </li>
+        <li className={` ${location.pathname === "/about" ? "Active" : ""}`}>
+          <Link to="/batman">About</Link>
+        </li>
+        <li className={` ${location.pathname === "/payment" ? "Active" : ""}`}>
+          <Link to="/payment">Payment</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const About = props => {
+  console.log(props.match);
+  return (
+    <div>
+      <Link to={`${props.match.url}/lol`}>lololo</Link>
+      <h1>About Page</h1>
+    </div>
+  );
+};
+
+const HomePage = props => {
+  //console.log(props);
+  let { id } = useParams();
+  if (props.location.pathname === "/") {
+    props.history.push("/profile");
+  }
+  return (
+    <div>
+      <h1>Estas en Home</h1>
+      {id && <p>Bienvenido: {id}</p>}
+    </div>
+  );
+};
+
+const Pay = () => {
+  return <h1>Payment page</h1>;
+};
 
 export default App;
